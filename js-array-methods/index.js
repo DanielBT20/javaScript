65a0b4688df483266f82128a5673a9fc442b1f9f
RaviBugendaitech <ravi.singh@bugendaitech.com>
2022-12-23 22:33:24 +0530

    js splice

--- js-array-methods/index.js
+++ js-array-methods/index.js
@@ -1,3 +1,15 @@
-const language = ["C", "C++", "Java", "Python"];
-const lang = language.slice(0, 3);
-console.log(lang);
+/**
+ * slice
+ */
+
+// const language = ["C", "C++", "Java", "Python"];
+// const lang = language.slice(0, 3);
+// console.log(lang);
+
+/**
+ * splice
+ */
+
+const number = [1, 2, 3, 4, 5];
+const num = number.splice(1, 3);
+console.log(num);
