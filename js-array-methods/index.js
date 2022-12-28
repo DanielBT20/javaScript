13b02f5157b9323a5836378b38c3215c34ad5470
RaviBugendaitech <ravi.singh@bugendaitech.com>
2022-12-28 23:49:45 +0530

    forEach array method

--- js-array-methods/index.js
+++ js-array-methods/index.js
@@ -10,6 +10,14 @@
  * splice
  */
 
-const number = [1, 2, 3, 4, 5];
-const num = number.splice(1, 3);
-console.log(num);
+// const number = [1, 2, 3, 4, 5];
+// const num = number.splice(1, 3);
+// console.log(num);
+
+/** forEach */
+
+const arr = [1, 2, 3, 4, 5];
+
+arr.forEach((data) => {
+  return console.log((data += 1));
+});
